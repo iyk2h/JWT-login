bba7177c8de4e4299acc74957a76a301973b79f8
youngkyunIm <yee0230@naver.com>
2022-02-03 22:01:19 +0900

    add security login

---
+++ security1/src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,33 @@
+package com.cos.security1.config.auth;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// 시큐리티 설정에서 loginProcessingUrl("/login");
+// login 요청이 오면 자동으로 UserDetailsService 타입으로 IoC 되어있는 loadUserByUsername 함수가 실행 (규칙)
+
+@Service
+public class PrincipalDetailsService implements UserDetailsService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //시큐리티 session = Authentication = UserDetails
+    // -> 시큐리티 session = Authentication(내부 UserDetails)
+    // -> 시큐리티 session(내부 Authentication(내부 UserDetails))
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("username: "+username);
+        User userEntity = userRepository.findByUsername(username);
+        if(userEntity != null) {
+            return new PrincipalDetails(userEntity);
+        }
+        return null;
+    }
+}
---
+++ security1/src/main/java/com/cos/security1/model/User.java
@@ -0,0 +1,28 @@
+package com.cos.security1.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "security_user")
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+	private String username;
+	private String password;
+	private String email;
+	private String role; //ROLE_USER, ROLE_ADMIN
+	@CreationTimestamp
+	private Timestamp createDate;
+}
